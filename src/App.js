f7c4b752e222cf8d34c00f0de933e3fd97462ebe
Aleksandr Ignatyev <vigntom@gmail.com>
2020-11-29 12:40:53 +0300

    Create application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__main">
+      </div>
+      <div className="app__controls">
+        <button className="btn" type="button">Модератор</button>
+        <button className="btn" type="button">Слушатель</button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
